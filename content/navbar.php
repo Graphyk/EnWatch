bba00bd5e668abff3dc234d8550a080c93a1ff4d
Graphyk <gueretlouis13@gmail.com>
2023-03-15 11:06:59 +0100

    navbar

---
+++ content/navbar.php
@@ -0,0 +1,22 @@
+<navbar class='bg-[#1CD4E1] h-[45px] px-[20px] inline-block flex justify-between items-center font-[MuseoModerno]'>
+    <div class='flex items-center'>
+        <div class='flex items-center'>
+        <div class='text-[40px] text-[#643CCE]'>EnWatch</div>
+        <img src="/ENWATCH-1/asset/img/logos/33312E.png" class='w-[35px] ml-[5px] h-[35px] inline-block'>
+        </div>
+        <div class='text-[24px] flex ml-[50px] gap-[30px]'>
+            <span class='flex gap-[5px]'>Catégorie<span class='inline-block -mt-[5px]'><span class='pr-[20px] pt-1 rotate-45 bg-black rounded-lg inline-block'></span><span class='pr-[20px]  rounded-lg -ml-[8.5px] pt-1 -rotate-45 bg-black inline-block'></span></span></span>
+            <span class='flex gap-[5px]'>Réalisateurs<span class='inline-block -mt-[5px]'><span class='pr-[20px] pt-1 rotate-45  rounded-lg bg-black inline-block'></span><span class='pr-[20px] -ml-[8.5px] pt-1 rounded-lg -rotate-45 bg-black inline-block'></span></span></span>
+            <span class='flex gap-[5px]'>Acteurs<span class='inline-block -mt-[5px]'><span class='pr-[20px] pt-1 rounded-lg rotate-45 bg-black inline-block'></span><span class='pr-[20px] -ml-[8.5px] pt-1 -rotate-45 rounded-lg bg-black inline-block'></span></span></span>
+        </div>
+    </div>
+    <div class='text-[16px] flex flex-col'>
+        <span>Inscription</span>
+        <span class='text-[#8E1CBB]'>Connexion</span>
+    </div>
+    <button id='OpenBtn' class='flex flex-col justify-evenly h-[25px] md:hidden'>
+        <span class='px-[10px] py-[1px] bg-white'></span>
+        <span class='px-[10px] py-[1px] bg-white'></span>
+        <span class='px-[10px] py-[1px] bg-white'></span>
+    </button>
+</navbar>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+    <link src='asset/style/reset.css' rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kodchasan:ital@0;1&family=MuseoModerno&family=Roboto&display=swap');
+    </style>
+</head>
+<body>
+    <?php
+include("content/navbar.php");
+?>
+</body>
+</html>
